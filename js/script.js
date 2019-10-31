81307a6d593f87ac63830228cb409d10e50a3e46
Alice Mwihaki <alicemwihaki99@gmail.com>
2019-10-31 13:03:23 +0300

    added logic to the programm

--- js/script.js
+++ js/script.js
@@ -7,20 +7,30 @@ submit.addEventListener("click", function(){
     const dayOfWeek = date.getDay();
     const male=document.getElementById("male");
     const female=document.getElementById("female");
-    const results=document.getElementById("result");
+    const results=document.getElementById("results");
 
     const days=["sunday","Monday","Tuesday","Wednesday","Thursday","Friday","saturday"];
     const maleName=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-    const femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    const femaleName=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
     // validation
     if(day==""){
-        alert("please enter day");
+        results.innerHTML = "please enter day";
+        results.style.color="red";
     } else if(month==""){
-        alert("please enter month");
+        results.innerHTML = "please enter month";
+        results.style.color="red";
     } else if(year==""){
-        alert("please enter year");
-    }
+        results.innerHTML = "please enter year";
+        results.style.color="red";
+    } else if(male.checked){
+        results.innerHTML="You were born on a " + days[dayOfWeek] + ". Your akan name is " + maleName[dayOfWeek];
+       results.style.color="blue";
+     } else {
+        results.innerHTML="You were born on a " + days[dayOfWeek] + ". Your akan name is " + femaleName[dayOfWeek];
+        results.style.color="blue";
+     }
+
 
 
 })
