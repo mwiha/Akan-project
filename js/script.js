ef29581a33d05aa755f8630318b76c535dceb662
Alice Mwihaki <alicemwihaki99@gmail.com>
2019-10-31 11:53:18 +0300

    added variables

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const submit=document.getElementById("submit");
+submit.addEventListener("click", function(){
+    const day=document.getElementById("day").value;
+    const month=document.getElementById("month").value;
+    const year=document.getElementById("year").value;
+    const male=document.getElementById("male");
+    const female=document.getElementById("female");
+
+    
+    
+})
