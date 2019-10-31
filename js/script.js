2a6178e6b6ef40ace1073829f12da27c8a5e750f
Alice Mwihaki <alicemwihaki99@gmail.com>
2019-10-31 12:14:25 +0300

    added date formula

--- js/script.js
+++ js/script.js
@@ -3,8 +3,12 @@ submit.addEventListener("click", function(){
     const day=document.getElementById("day").value;
     const month=document.getElementById("month").value;
     const year=document.getElementById("year").value;
+    const date= new Date(`${year}-${month}-${day}`);
+    const dayOfWeek = date.getDay();
     const male=document.getElementById("male");
     const female=document.getElementById("female");
+
+    
 
     
     
